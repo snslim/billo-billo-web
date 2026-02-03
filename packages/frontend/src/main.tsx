eee811307801c724d05274fec456e4cfab9c6cb1
harim lee <harimlee9506@gmail.com>
2026-02-03 22:06:33 +0900

    feat: Toast 알림 설정

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ErrorBoundary } from 'react-error-boundary';
+import { Toaster } from 'react-hot-toast';
 import { ErrorFallback } from './components/ErrorFallback';
 import './index.css';
 import App from './App';
@@ -9,6 +10,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary FallbackComponent={ErrorFallback}>
       <App />
+      <Toaster position="top-center" />
     </ErrorBoundary>
   </StrictMode>,
 );
