70b70ed675616503ac2997f8c9b841f545be18c6
harim lee <harimlee9506@gmail.com>
2026-03-10 01:45:24 +0900

    feat: AsyncLocalStorage로 requestId를 서비스 레이어까지 전파

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import { errorHandler } from './middlewares/errorHandler.js';
+import { requestContext } from './middlewares/requestContext.js';
 import { requestLogger } from './middlewares/requestLogger.js';
 import { healthRoutes } from './routes/healthRoutes.js';
 import { ocrRoutes } from './routes/ocrRoutes.js';
@@ -17,6 +18,7 @@ const ALLOWED_ORIGINS = [process.env.FRONTEND_URL, 'http://localhost:5173'].filt
 app.use(helmet());
 app.use(cors({ origin: ALLOWED_ORIGINS }));
 app.use(express.json());
+app.use(requestContext);
 app.use(requestLogger);
 
 app.use('/api', healthRoutes);
